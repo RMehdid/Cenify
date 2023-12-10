fe6776da92cd4f138d5c0babc786880b500d86dc
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-12-10 11:30:13 +0100

    Initial Commit

---
+++ Cenify/CenifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenifyApp.swift
+//  Cenify
+//
+//  Created by Samy Mehdid on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CenifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
