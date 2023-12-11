b20636e76e3ba4107c75e3e850499aa3e6caef62
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-12-11 10:16:40 +0100

    initialized loading view

---
+++ Cenify/Components/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Cenify
+//
+//  Created by Samy Mehdid on 11/12/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
